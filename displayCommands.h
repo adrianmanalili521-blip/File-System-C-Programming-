7fa0f4e3e528ddfda8a70676a7935399be4fafe8
adrian <adrianmanalili521@gmail.com>
2026-03-23 21:15:13 +0800

    added icon updated display command colors

--- displayCommands.h
+++ displayCommands.h
@@ -1,13 +1,21 @@
 #ifndef DISPLAYCOMMANDS_H
 #define DISPLAYCOMMANDS_H
 
+#include "myColors.h"
+
 void displayCommands() {
-    printf("\nls: used to list folders in current directory\n");
-    printf("mkdir: used to create folder\n"); 
-    printf("touch: used to create file");
-    printf("cd: used to enter a directory\n");
-    printf("cd ..: used to get back to previous directory\n");
-    printf("cd ~: used to get to root directory\n\n");
+    printf(CYAN "\nls" RESET);
+    printf(": used to list folders in current directory\n");
+    printf(CYAN "mkdir" RESET);
+    printf(": used to create folder\n"); 
+    printf(CYAN "touch" RESET);
+    printf(": used to create file");
+    printf(YELLOW "cd" RESET);
+    printf(": used to enter a directory\n");
+    printf(YELLOW "cd .." RESET);
+    printf(": used to get back to previous directory\n");
+    printf(YELLOW "cd ~" RESET);
+    printf(": used to get to root directory\n\n");
 }
 
 #endif
